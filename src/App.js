f8a623a08af2993dc9b911261bf4e359c887b9e8
melvinkukwansafari <melvinkukwan98@gmail.com>
2023-12-09 10:09:41 +0300

    work progress

--- src/App.js
+++ src/App.js
@@ -2,27 +2,58 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
-import AboutUs from './components/AboutUs';  // Fix the import statement
-import Contact from './components/Contact';
+import AboutUs from './components/AboutUs';
+//import Contact from './components/Contact';
 import Museum from './components/Museum';
+import Gallery from './components/Gallery';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <Router>
-      <Layout>
-        {/* Replace Switch with Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />  {/* Use AboutUs instead of About */}
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/museum" element={<Museum />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/about"
+          element={
+            <Layout>
+              <AboutUs />
+            </Layout>
+          }
+        />
+       
+        <Route
+          path="/museum"
+          element={
+            <Layout>
+              <Museum />
+            </Layout>
+          }
+        />  
+
+      <Route
+          path="/gallery"
+          element={
+            <Layout>
+              <Gallery />
+            </Layout>
+          }
+        />
+
+      </Routes>
     </Router>
   );
 };
 
 export default App;
+
 
 
 
--- src/components/Museum.js
+++ src/components/Museum.js
@@ -1,40 +1,87 @@
-// src/components/Museum.js
-//import React, { useState, useEffect } from 'react';
-//import axios from 'axios';
-import artifactsData from '../db.json';
-
+import React, { useState, useEffect } from 'react';
+import Image1 from './flowers.jpg'; // Import the image file
+import Image2 from './samburu-hand-knife.jpg';
+import Image3 from './moran.jpg';
 const Museum = () => {
-    const artifacts = artifactsData.artifacts;
-    const gallery = artifactsData.gallery;
+  const [artifacts, setArtifacts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('all');
 
-  //useEffect(() => {
-    //const fetchData = async () => {
-      //const result = await axios.get('.../public/db.json');
-      //setArtifacts(result.data.artifacts);
-      //setGallery(result.data.gallery);
-    //};
+  const localArtifactsData = [
+    {
+      "id": 1,
+      "name": "Natural Adherence",
+      "description": "Samburu got and adopt ways to live with the wild and up to this age still practising it",
+      "category": "mammalia",
+      "image": Image3
+    },
+    {
+      "id": 2,
+      "name": "Samburu Flowers",
+      "description": "This is a flower occuring from acacia plants. A dry land tree with thorns but beatiful flowers",
+      "category": "plants",
+      "image": Image1 // Use the imported image
+    },
+    {
+      "id": 3,
+      "name": "Samburu-hand-knife",
+      "description": "This is a tool that was used by samburu people, as it was helpful to them with activity like; Goat eating, gathering and circumcision",
+      "category": "arts",
+      "image": Image2 // Use the imported image
+    }
+    // Add more artifacts with different categories and images
+  ];
 
-    //fetchData();
-  //}, []);
+  useEffect(() => {
+    // Set artifacts directly from local data
+    setArtifacts(localArtifactsData);
+  }, [localArtifactsData]); // Include localArtifactsData in the dependency array
+
+  const filteredArtifacts = selectedCategory === 'all'
+    ? artifacts
+    : artifacts.filter(artifact => artifact.category === selectedCategory);
 
   return (
-    <div>
-      <h2>Artifacts</h2>
-      {artifacts.map((artifact) => (
-        <div key={artifact.id}>
-          <p>{artifact.name}</p>
-          <p>{artifact.description}</p>
-        </div>
-      ))}
-      <h2>Gallery</h2>
-      {gallery.map((item) => (
-        <div key={item.id}>
-          {/* Render gallery data */}
-          {/* ... */}
-        </div>
-      ))}
+    <div className="container mt-4">
+      <label>
+        Select Category:
+        <select
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          className="form-control"
+        >
+          <option value="all">All</option>
+          <option value="mammalia">Mammalia</option>
+          <option value="plants">Plants</option>
+          <option value="arts">Arts</option>
+          {/* Add more categories as needed */}
+        </select>
+      </label>
+
+      <h2 className="mt-3">Artifacts</h2>
+      <div className="row">
+        {filteredArtifacts.map((artifact) => (
+          <div key={artifact.id} className="col-md-12 mb-3"> {/* Use col-md-12 to take up the full width */}
+            <div className="card">
+              <img src={artifact.image} className="card-img-top" alt={`Artifact ${artifact.id}`} />
+              <div className="card-body">
+                <h5 className="card-title">{artifact.name}</h5>
+                <p className="card-text">{artifact.description}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Museum;
+
+
+
+
+
+
+
+
+
