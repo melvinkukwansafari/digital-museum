dc376692c7779ca6d25fe00b62a41230a147cf6d
melvinkukwansafari <melvinkukwan98@gmail.com>
2023-12-11 12:36:52 +0300

    navigation

--- src/components/Museum.js
+++ src/components/Museum.js
@@ -6,35 +6,36 @@ const Museum = () => {
   const [artifacts, setArtifacts] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState('all');
 
-  const localArtifactsData = [
-    {
-      "id": 1,
-      "name": "Natural Adherence",
-      "description": "Samburu got and adopt ways to live with the wild and up to this age still practising it",
-      "category": "mammalia",
-      "image": Image3
-    },
-    {
-      "id": 2,
-      "name": "Samburu Flowers",
-      "description": "This is a flower occuring from acacia plants. A dry land tree with thorns but beatiful flowers",
-      "category": "plants",
-      "image": Image1 // Use the imported image
-    },
-    {
-      "id": 3,
-      "name": "Samburu-hand-knife",
-      "description": "This is a tool that was used by samburu people, as it was helpful to them with activity like; Goat eating, gathering and circumcision",
-      "category": "arts",
-      "image": Image2 // Use the imported image
-    }
-    // Add more artifacts with different categories and images
-  ];
+  
 
   useEffect(() => {
+    const localArtifactsData = [
+      {
+        "id": 1,
+        "name": "Natural Adherence",
+        "description": "Samburu got and adopt ways to live with the wild and up to this age still practising it",
+        "category": "mammalia",
+        "image": Image3
+      },
+      {
+        "id": 2,
+        "name": "Samburu Flowers",
+        "description": "This is a flower occuring from acacia plants. A dry land tree with thorns but beatiful flowers",
+        "category": "plants",
+        "image": Image1 // Use the imported image
+      },
+      {
+        "id": 3,
+        "name": "Samburu-hand-knife",
+        "description": "This is a tool that was used by samburu people, as it was helpful to them with activity like; Goat eating, gathering and circumcision",
+        "category": "arts",
+        "image": Image2 // Use the imported image
+      }
+      // Add more artifacts with different categories and images
+    ];
     // Set artifacts directly from local data
     setArtifacts(localArtifactsData);
-  }, [localArtifactsData]); // Include localArtifactsData in the dependency array
+  }, []); // Include localArtifactsData in the dependency array
 
   const filteredArtifacts = selectedCategory === 'all'
     ? artifacts
@@ -57,7 +58,7 @@ const Museum = () => {
         </select>
       </label>
 
-      <h2 className="mt-3">Artifacts</h2>
+      <h2 className="mt-3">Enjoy Our Museum</h2>
       <div className="row">
         {filteredArtifacts.map((artifact) => (
           <div key={artifact.id} className="col-md-12 mb-3"> {/* Use col-md-12 to take up the full width */}
